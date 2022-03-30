5b63150f87202e4be5ef885f3c0e5691baec5136
NaGaaDEV <NaGaaDEV@gmail.com>
2022-03-29 19:38:16 -0500

    Lab02-express

---
+++ Lab02-express/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+require('dotenv').config();
+
+const app = express();
+
+app.post("/", function(req, res) {
+    res.status(200).json({"message" : "JSON"});
+})
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("port", process.env.PORT)
+const server = app.listen(app.get("port"), function() {
+    console.log(process.env.SERVER_RUNNING_ON_MSG, server.address().port);
+})
